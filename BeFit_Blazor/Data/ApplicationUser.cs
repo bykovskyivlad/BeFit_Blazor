fe0cd6acf576dc7da011f304e8a2719c3891e700
Vladyslav <bikovskijvladislav@gmail.com>
2025-11-26 16:01:17 +0100

    first commit

---
+++ BeFit_Blazor/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BeFit_Blazor.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
